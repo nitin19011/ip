7a159e8e34748be2d20bc0baa81f4d6802ae659e
nitin <nitinsaklani18@gmail.com>
2025-08-26 19:04:23 +0800

    Duke's name change to Socks; Added greeting & exit
    Implemented initial skeleton for the Socks chatbot:
    - Added greeting message
    
    - Added exit message

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Socks User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Socks.java
@@ -0,0 +1,17 @@
+public class Socks {
+
+
+    public static void main(String[] args) {
+        String logo =
+                "  ____             _      ____   \n"
+                        + " / ___|  ___   ___| | __ / ___|  \n"
+                        + " \\___ \\ / _ \\ / __| |/ / \\___ \\  \n"
+                        + "  ___) | (_) | (__|   <   ___) | \n"
+                        + " |____/ \\___/ \\___|_|\\_\\ |____/  \n";
+
+        System.out.println("Hello from\n" + logo + "Your trusty life tracker! What can I do for you?");
+
+        System.out.println("Stay productive and hope to see you again soon!");
+
+    }
+}
