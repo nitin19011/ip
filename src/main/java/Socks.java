1029505549dd8757f38754539839d3e4e74e91e6
nitin <nitinsaklani18@gmail.com>
2025-08-29 00:11:44 +0800

    Added Sock's ability to mark and unmark tasks.
    Socks is also able to handle user input errors like
    - invalid mark
    - invalid unmark

--- src/main/java/Socks.java
+++ src/main/java/Socks.java
@@ -19,12 +19,32 @@ public class Socks {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String userInput = sc.nextLine();
+            String[] parts = userInput.split(" ");
             if (userInput.equalsIgnoreCase("bye")) { // handle exit command
                 System.out.println("    ByeBye, Stay productive and hope to see you again soon!");
                 break;
-            }
-            if (userInput.equalsIgnoreCase("list")) { // handle listing of the list including when its empty
+            } else if (userInput.equalsIgnoreCase("list")) { // handle listing of the list including when its empty
                 toDoList.listTasks();
+            } else if (parts.length == 2 && (parts[0].equalsIgnoreCase("mark") || parts[0].equalsIgnoreCase("unmark"))) {
+                try {
+                    int taskNumber = Integer.parseInt(parts[1]);
+                    if (taskNumber < 1 || taskNumber > toDoList.getSize()) {
+                        System.out.println("    Task number " + taskNumber + " does not exist.");
+                    } else {
+                        if (parts[0].equalsIgnoreCase("mark")) {
+                            toDoList.markDone(taskNumber);
+                            System.out.println(" Nice! I've marked this task as done:");
+                            toDoList.listTask(taskNumber);
+                        } else if (parts[0].equalsIgnoreCase("unmark")) {
+                            toDoList.unmarkDone(taskNumber);
+                            System.out.println(" Sure, I'll unmarked this task:");
+                            toDoList.listTask(taskNumber);
+                        }
+                    }
+                } catch (NumberFormatException error) {
+                    toDoList.addTask(userInput);
+                    System.out.println("    Added: " + userInput);
+                }
             } else {
                 toDoList.addTask(userInput); // add the user's input to list and show what was added to user
                 System.out.println("    Added: " + userInput);
